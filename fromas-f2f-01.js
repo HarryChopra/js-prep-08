8f7bd75aca9b31e4ba86e07f1ca24cc9989174e3
Harry Chopra <harrychopra@gmail.com>
2020-02-26 23:34:50 +0000

    Done

--- fromas-f2f-01.js
+++ fromas-f2f-01.js
@@ -1,3 +1,21 @@
+// Dot Assignment
 
+var person = {};
+person.name = 'Mrs. White';
+person; //{ name: 'Mrs. White' }
 
+var myArr = [];
+console.log(myArr.length);
+myArr.customFn = 'Hello!';
+myArr.push(10);
+console.log(myArr); // [ 10, customFn: 'Hello!' ]
 
+// Access with Dots
+
+var who = person.name;
+console.log(who); // Mrs. White
+
+// Reassigning the original memory space
+// The content of 'Who' remains the same, because there is a difference between storing by reference and by value
+person.name = 'Mr. White';
+console.log(who); // Mrs. White
