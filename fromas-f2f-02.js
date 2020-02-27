c0e521cb763bd408d9f60e95b0f23beb2ce9a8de
Harry Chopra <harrychopra@gmail.com>
2020-02-27 02:55:57 +0000

    Done

--- fromas-f2f-02.js
+++ fromas-f2f-02.js
@@ -26,3 +26,41 @@ function foobar() {
 }
 
 foobar(); // Miss Scarlet
+
+// Destructure this nested data structure into two variables with the strings red and orange
+var { color1, color2 } = { color1: game.suspects[0].color, color2: game.suspects[1].color };
+color1; // orange
+color2; // red
+
+var [{ color: color3 }, { color: color4 }] = game.suspects;
+color3; //orange
+color4; // red
+
+// List transformation
+
+function CreateSuspectObjects(name) {
+	return {
+		name: name,
+		color: name.split(' ')[1],
+		speak() {
+			console.log(`My name is ${name}`);
+		}
+	};
+}
+
+var suspects = ['Miss Scarlet', 'Colonel Mustard', 'Mr. White'];
+
+var suspectList = [];
+
+function createSuspectList() {
+	for (let suspect of suspects) {
+		suspectList.push(CreateSuspectObjects(suspect));
+	}
+}
+
+createSuspectList();
+console.log(suspectList);
+// [ { name: 'Miss Scarlet', color: 'Scarlet', speak: [λ: speak] },
+//  { name: 'Colonel Mustard', color: 'Mustard', speak: [λ: speak] },
+//  { name: 'Mr. White', color: 'White', speak: [λ: speak] } ]
+suspectList[1].speak(); // My name is Colonel Mustard
